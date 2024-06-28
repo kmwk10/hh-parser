3a94e5e8b3516989dc9e3597bbd2041551560a35
kk10 <kamy.zero.10@gmail.com>
2024-06-28 21:55:57 +0300

    Added functions for getting parsing results

--- Backend/src/main.py
+++ Backend/src/main.py
@@ -1,31 +1,38 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
- 
+from src.db import create_tables, update_vacancies_table, update_resumes_table
+from src.parsing import get_vacancy_data, get_vacancy, get_resume_links, get_resume
+
 app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    try:
+        create_tables()
+        return "Таблица создалась"
+    except:
+        return "Ошибка"
 
 @app.get("/vacancies")
-def gat_vacancies(text: str):
-    return text
+def gat_vacancies(text: str, count: int):
+    try:
+        data = []
+        for item in get_vacancy_data(text, count):
+            vacancy = get_vacancy(item)
+            data.append(vacancy)
+            update_vacancies_table(vacancy)
+        return data
+    except Exception as e:
+        return f"{e}"
 
 @app.get("/resumes")
-def gat_resumes(text: str):
-    return text
-
-class Vacancy(BaseModel):
-        id: str
-        name: str
-        salary: str
-        area: str
-        schedule: str
-        employment: str
-        experience: str
-        requirement: str
-        employer : str
-
-# @app.post("/vacancies")
-# def add_vacancies(vacancies: List[Vacancy]):
-#      return {"status": 200, "data": vacancies}
+def gat_resumes(text: str, count: int):
+    try:
+        data = []
+        for link in get_resume_links(text, count):
+            resume = get_resume(link)
+            data.append(resume)
+            update_resumes_table(resume)
+        return data
+    except Exception as e:
+        return f"{e}"
